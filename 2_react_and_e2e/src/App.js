4239fa7eb63d4ae87666d6e0d355850b840fe602
Aliaksandr Senkevich <puskin1914@gmail.com>
2022-02-23 14:02:53 +0300

    test react markup

---
+++ 2_react_and_e2e/src/App.js
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>Hello world</h1>
+      <button>click me</button>
+      <input placeholder="input value"></input>
+    </div>
+  );
+}
+
+export default App;
