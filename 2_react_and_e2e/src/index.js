61a4a9e5207d85ce3e688952bffa3a12f08e5c7c
Aliaksandr Senkevich <puskin1914@gmail.com>
2022-03-05 14:47:13 +0300

    add redux functionality

--- 2_react_and_e2e/src/index.js
+++ 2_react_and_e2e/src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
+import { createReduxStore } from './store/store';
 import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <Provider store={createReduxStore()}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
