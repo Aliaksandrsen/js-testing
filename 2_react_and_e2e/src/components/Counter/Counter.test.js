998890b9942a49f74306bdbee1701a3e0eac40d4
Aliaksandr Senkevich <puskin1914@gmail.com>
2022-03-08 19:00:55 +0300

    test router+redux (renderTestApp helper)

--- 2_react_and_e2e/src/components/Counter/Counter.test.js
+++ 2_react_and_e2e/src/components/Counter/Counter.test.js
@@ -2,12 +2,17 @@ import { render, screen } from '@testing-library/react';
 import Counter from './Counter';
 import userEvent from '@testing-library/user-event';
 
-import { renderWithRedux } from '../../tests/helpers/renderWithRedux';
+import { renderTestApp } from '../../tests/helpers/renderTestApp';
 
 describe('Counter test', () => {
   test('router', async () => {
     const { getByTestId } = render(
-      renderWithRedux(<Counter />, { counter: { value: 10 } })
+      renderTestApp(null, {
+        route: '/',
+        initialState: {
+          counter: { value: 10 },
+        },
+      })
     );
 
     const incrementBtn = screen.getByTestId('increment-btn');
